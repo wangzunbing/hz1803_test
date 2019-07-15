54b8aeac1904bb9ef3508362299b017b269da373
wangzunbing <wzb18325716719@163.com>
2019-07-15 19:50:15 +0800

    monday

--- src/main/scala/com/Spark_DW/DWS_DM.scala
+++ src/main/scala/com/Spark_DW/DWS_DM.scala
@@ -3,20 +3,21 @@ package com.Spark_DW
 import com.Constans.Constan
 import com.SparkUtils.JDBCUtils
 import com.config.ConfigManager
-import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkConf, SparkContext, sql}
 import org.slf4j.LoggerFactory
 
 /**
   * dwd导入dws
   */
-object DWD_DWS {
+object DWS_DM {
   def main(args: Array[String]): Unit = {
 //    System.setProperty("hadoop.home.dir", "F:\\soft\\hadoop-common-2.2.0-bin-master.zip\\hadoop-common-2.2.0-bin-master")
-    val conf = new SparkConf().setAppName(Constan.SPARK_APP_NAME_USER).setMaster(Constan.SPARK_LOACL)
-    val sc = new SparkContext(conf)
-    val hiveContext = new HiveContext(sc)
+//    val conf = new SparkConf().setAppName(Constan.SPARK_APP_NAME_USER).setMaster(Constan.SPARK_LOACL)
+//    val sc = new SparkContext(conf)
+//    val hiveContext = new HiveContext(sc)
+    val hiveContext = SparkSession.builder().appName("SPARK_APP_NAME_USER").master("local").enableHiveSupport().config("spark.debug.maxToStringFileds","100").getOrCreate()
     // 加载相应的语句
     val sql = ConfigManager.getProper(args(0))
     if(sql == null){
@@ -24,7 +25,7 @@ object DWD_DWS {
         .debug("提交的表名参数有问题！请重新设置。。。")
     }else{
       // 处理SQL内部的占位符
-//      val finalSql = sql.replace("?",args(1))
+      val finalSql = sql.replace("?",args(1))
       // 运行SQL
       val df = hiveContext.sql(sql)
       // 处理配置参数
@@ -33,7 +34,8 @@ object DWD_DWS {
       val jdbcProp = JDBCUtils.getJdbcProp()._1
       val jdbcUrl = JDBCUtils.getJdbcProp()._2
       // 存入MySQL
-       df.write.mode("overwrite").jdbc(jdbcUrl,mysqlTableName,jdbcProp)
+      df.show()
+       df.write.mode(SaveMode.Append).jdbc(jdbcUrl,mysqlTableName,jdbcProp)
       // 存入Hive
 //      df.write.mode(SaveMode.Overwrite).insertInto(hiveTableName)
     }
--- src/main/scala/com/config/ConfigManager.scala
+++ src/main/scala/com/config/ConfigManager.scala
@@ -7,7 +7,7 @@ object ConfigManager {
   private val prop = new Properties()
   // 通过类加载器方法来加载指定的配置文件
   try{
-    val in_dws = ConfigManager.getClass.getClassLoader.getResourceAsStream("dwd_dws.properties")
+    val in_dws = ConfigManager.getClass.getClassLoader.getResourceAsStream("dws_dm.properties")
 
     val in_basic = ConfigManager.getClass.getClassLoader.getResourceAsStream("basic.properties")
 
